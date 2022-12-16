305f3a603a55cd54ac300a5cb97639e15ecaa508
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2022-12-16 14:41:45 +0545

    file name issue

--- routes/Blog.js
+++ routes/Blog.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const blogController = require("../controllers/blogController");
 
+// get blog list
 router.get("/", blogController.getBlogs);
 
 module.exports = router;
